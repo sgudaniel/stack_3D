9967b5bec27a936fbe8f7b62a207e4176dacc5b5
Gregorius Ferdinand <ferdinandgregorius@yahoo.com>
2022-12-02 10:51:47 +0700

    Added Increase() function block in ScoreCounterComponent.cs. Line 31

--- Assets/Scripts/ScoreCounter/ScoreCounterComponent.cs
+++ Assets/Scripts/ScoreCounter/ScoreCounterComponent.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
 using System;
+using TMPro;
 
 public class ScoreCounterComponent : MonoBehaviour
 {
     private Rigidbody rigidbody;
     private Subject<Unit> theSubject;
+    private TextMeshProUGUI scoreText;
 
     public IObservable<Unit> GetObservable
     {
@@ -24,6 +26,12 @@ public class ScoreCounterComponent : MonoBehaviour
     void Start()
     {
         this.rigidbody = this.gameObject.GetComponent<Rigidbody>();
-    }    
+    }
+
+    void Increase()
+    {
+        int scoreInteger = int.Parse(this.scoreText.text + 1);
+        this.scoreText.text = scoreInteger.ToString();
+    }
 
 }
