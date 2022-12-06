d8e480a387acc21f1649e02c2e2bfdf646381c97
sgudaniel <daniel.amazia@student.sgu.ac.id>
2022-12-06 14:48:30 +0700

    Update EventModel

--- Assets/Scripts/Core/EventModel.cs
+++ Assets/Scripts/Core/EventModel.cs
@@ -5,18 +5,13 @@ using System.Collections.Generic;
 public class EventModel
 {
     public string msg;
-    public int CubeIndex;
-    public int LastCubeHeight;
-    public int Score;
 
-    public EventModel(string msg, int cubeindex, int lastcubeheight, int score){
+    public EventModel(string msg){
         this.msg = msg;
-        this.CubeIndex = cubeindex;
-        this.LastCubeHeight = lastcubeheight;
-        this.Score = score;
+        
     }
 
     public static EventModel getDefaultInstance(){
-        return new EventModel("default",0,0,0);
+        return new EventModel("default");
     }
 }
