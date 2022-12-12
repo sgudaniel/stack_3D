5092c3c0e6f83b0d0e8ecc057e7113d6e0ea4599
sgudaniel <daniel.amazia@student.sgu.ac.id>
2022-12-12 13:45:07 +0700

    Add gameover logic

--- Assets/Scripts/Startup/StartupComponent.cs
+++ Assets/Scripts/Startup/StartupComponent.cs
@@ -6,78 +6,90 @@ using UniRx;
 
 public class StartupComponent : MonoBehaviour
 {
-    
+
     private ScoreCounterComponent scoreCounterComponent;
     private List<StackComponent> stackComponents = new List<StackComponent>();
     private EventPublisher evPublisher = EventPublisher.getInstance();
     private Logger logger = Logger.getInstance();
     private StackFactory stackFactory = new StackFactory();
-    
+
     private Subject<Unit> theSubject = new Subject<Unit>();
 
 
     StartupComponent()
     {
-        
-        
+
+
     }
-    
+
     void Start()
     {
         var scoreCounterFactory = new ScoreCounterFactory();
 
-        var inFrontOfCamera =  CameraUtil.inFront(Camera.main);
-        var cameraRotation = CameraUtil.inFrontRotation(Camera.main);        
+        var inFrontOfCamera = CameraUtil.inFront(Camera.main);
+        var cameraRotation = CameraUtil.inFrontRotation(Camera.main);
 
         this.scoreCounterComponent = scoreCounterFactory.Create(inFrontOfCamera, cameraRotation);
 
-        StackComponent firstStack = stackFactory.CreateFirstStack(new Vector3(0,GameState.CurrentCubeHeight,0), false);
+        StackComponent firstStack = stackFactory.CreateFirstStack(new Vector3(0, GameState.CurrentCubeHeight, 0), false);
         this.stackComponents.Add(firstStack);
-        
+
         GameState.increaseState(firstStack);
 
-        this.stackComponents.Add(stackFactory.Create(new Vector3(0,GameState.CurrentCubeHeight,0), false, GameState.getPrevStackLocalScale()));
+        this.stackComponents.Add(stackFactory.Create(new Vector3(GameState.BOUNDARY, GameState.CurrentCubeHeight, 0), false, GameState.getPrevStackLocalScale()));
         //this.stackComponent.Add(stackFactory.Create(new Vector3(0,1.2f,0), false));
 
         this.RegisterEvents();
-        
+
     }
 
-    
+
     void Update()
     {
-        
+
     }
 
     private void RegisterEvents()
     {
-        evPublisher.Event.Where(x=> x.msg == "click").Subscribe(_=>{
+        evPublisher.Event.Where(x => x.msg == "click" && !GameState.GameOver).Subscribe(_ =>
+        {
             StackComponent curStack = this.stackComponents[GameState.CubeIndex];
             StackComponent prevStack = this.stackComponents[GameState.CubeIndex - 1];
-            curStack.stop();
+            
 
             var curStackTransform = curStack.transform;
             var prevStackTransform = prevStack.transform;
 
             var deltaX = Mathf.Abs(prevStackTransform.position.x - curStackTransform.position.x);
-            var middle = prevStackTransform.position.x + curStackTransform.position.x /2;
+            var middle = prevStackTransform.position.x + curStackTransform.position.x / 2;
             var xpos = middle - (prevStackTransform.position.x / 2);
-            
-            GameState.changeStackBounds(new Vector2(GameState.stackBounds.x-  deltaX ,GameState.stackBounds.y));
 
-            if(GameState.stackBounds.x <= 0){
-                return;
+            GameState.changeStackBounds(new Vector2(GameState.StackBounds.x - deltaX, GameState.StackBounds.y));
+
+            GameState.increaseState(curStack);
+            this.scoreCounterComponent.Increase();
+
+            if (GameState.StackBounds.x <= 0)
+            {
+                curStack.drop();
+                GameState.GameisOver();
+            }
+            else
+            {
+                curStack.stop();
+                curStack.CreateRubble(curStackTransform, deltaX);
+                curStack.resize(new Vector3(xpos, curStackTransform.position.y, curStackTransform.position.z), new Vector3(GameState.StackBounds.x, curStackTransform.localScale.y, curStackTransform.localScale.z));
+
+                
+                this.stackComponents.Add(stackFactory.Create(new Vector3(GameState.BOUNDARY, GameState.CurrentCubeHeight, 0), false, GameState.getPrevStackLocalScale()));
+                
             }
 
-            curStack.CreateRubble(curStackTransform,deltaX);
-            curStack.resize(new Vector3(xpos, curStackTransform.position.y, curStackTransform.position.z), new Vector3(GameState.stackBounds.x, curStackTransform.localScale.y, curStackTransform.localScale.z));
-
             
-            GameState.increaseState(curStack);
-            this.stackComponents.Add(stackFactory.Create(new Vector3(0,GameState.CurrentCubeHeight,0), false, GameState.getPrevStackLocalScale()));
-            this.scoreCounterComponent.Increase();
+
+
         });
-        
-        
+
+
     }
 }
