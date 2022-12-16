fbdee728e35465b03db7b02386546831375bef3b
sgudaniel <daniel.amazia@student.sgu.ac.id>
2022-12-16 09:46:04 +0700

    Fix naming convention in GameState

--- Assets/Scripts/State/GameState.cs
+++ Assets/Scripts/State/GameState.cs
@@ -11,31 +11,31 @@ public static class GameState
 
     
 
-    private static int _CubeIndex = 0;
-    private static int _Score  = 0;
-    private static int _Combo = 0;
-    private static float _StackSpeed = 1f;
-    private static float  _CurrentCubeHeight = 0.58f;
+    private static int _cubeIndex = 0;
+    private static int _score  = 0;
+    private static int _combo = 0;
+    private static float _stackSpeed = 1f;
+    private static float  _currentCubeHeight = 0.58f;
     
-    private static bool _StackReverseMove = false;
-    private static bool _GameOver = false;
+    private static bool _stackReverseMove = false;
+    private static bool _gameOver = false;
 
-    private static StackComponent prevStack;
+    private static StackComponent _prevStack;
 
-    private static Vector2 _StackBounds = new Vector2(BOUNDS_SIZE, BOUNDS_SIZE);
+    private static Vector2 _stackBounds = new Vector2(BOUNDS_SIZE, BOUNDS_SIZE);
 
     
 
-    public static int CubeIndex { get => _CubeIndex; set => _CubeIndex = value; }
-    public static int Score { get => _Score; set => _Score = value; }
-    public static int Combo { get => _Combo; set => _Combo = value; }
-    public static float StackSpeed { get => _StackSpeed; set => _StackSpeed = value; }
-    public static float CurrentCubeHeight { get => _CurrentCubeHeight; set => _CurrentCubeHeight = value; }
-    public static bool GameOver { get => _GameOver;}
-    public static StackComponent PrevStack { get => prevStack; set => prevStack = value; }
-    public static Vector2 StackBounds { get => _StackBounds;}
+    public static int CubeIndex { get => _cubeIndex; set => _cubeIndex = value; }
+    public static int Score { get => _score; set => _score = value; }
+    public static int Combo { get => _combo; set => _combo = value; }
+    public static float StackSpeed { get => _stackSpeed; set => _stackSpeed = value; }
+    public static float CurrentCubeHeight { get => _currentCubeHeight; set => _currentCubeHeight = value; }
+    public static bool GameOver { get => _gameOver;}
+    public static StackComponent PrevStack { get => _prevStack; set => _prevStack = value; }
+    public static Vector2 StackBounds { get => _stackBounds;}
 
-    public static bool StackReverseMove { get => _StackReverseMove;}
+    public static bool StackReverseMove { get => _stackReverseMove;}
 
     public static void increaseState(StackComponent sc){
         CubeIndex += 1;
@@ -49,15 +49,15 @@ public static class GameState
     }
 
     public static void changeStackBounds(Vector2 sb){
-        _StackBounds = sb;
+        _stackBounds = sb;
     }
 
     public static void GameisOver(){
-        _GameOver = true;
+        _gameOver = true;
     }
 
     public static void ReverseStackMove(){
-        _StackReverseMove = !_StackReverseMove;
+        _stackReverseMove = !_stackReverseMove;
     }
 
     
