f4337a520ad2a8b457f6e3f5855724aaab91d916
Gregorius Ferdinand <ferdinandgregorius@yahoo.com>
2023-01-06 08:44:29 +0700

    Added QuitGame() function block from line 17 to 21 in MainMenu.cs

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -12,6 +12,11 @@ public class MainMenu : MonoBehaviour
     {
         MainThreadDispatcher.Initialize();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
     }
 }
