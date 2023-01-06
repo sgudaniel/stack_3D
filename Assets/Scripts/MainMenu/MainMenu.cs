c775c8d5e60fdd15ef1b405c00c7d96732225d44
Gregorius Ferdinand <ferdinandgregorius@yahoo.com>
2023-01-06 08:33:17 +0700

    Main menu created, and can redirect to the main game scene when the "Play" button is clicked

---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UniRx;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        MainThreadDispatcher.Initialize();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        
+    }
+}
